b4773054476f3373aa7eacc98f94df7ab347563d
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-12 22:45:38 +0000

    not fibbonacci

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,21 @@ namespace Special
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Insert n-th number of the special sequence:");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+
+            Console.WriteLine(GetSpecial(num));
+        }
+        
+        private static int GetSpecial(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
         }
     }
 }
