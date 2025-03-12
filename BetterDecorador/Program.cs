bd2580a41d6c53554d3dede280067acaad5d16fe
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-12 21:51:20 +0000

    comentários adicionados

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -27,12 +27,17 @@ namespace BetterDecorador
             }
         }  
         /// <summary>
-        /// Utilizes arguments to create a string with a character dec times num on each side of a string s
+        /// Utilizes arguments to create a string with a character dec times num
+        ///  on each side of a string s
         /// </summary>
-        /// <param name="s">A string received from the arguments in the command line</param>
-        /// <param name="dec">A char received from the arguments in the command line</param>
-        /// <param name="num">An int received from the arguments in the command line</param>
-        /// <returns>a string composed by a character dec times num on each side of a string s</returns>
+        /// <param name="s">A string received from the arguments in the command 
+        /// line</param>
+        /// <param name="dec">A char received from the arguments in the command 
+        /// line</param>
+        /// <param name="num">An int received from the arguments in the command 
+        /// line</param>
+        /// <returns>a string composed by a character dec times num on each side 
+        /// of a string s</returns>
         private static string Decor(string s, char dec, int num)
         {
             
@@ -40,7 +45,13 @@ namespace BetterDecorador
 
             return $"{a} {s} {a}";
         } 
-
+        /// <summary>
+        /// Utilizes the decor method to print a string that lets the user know 
+        /// they didn't specify any arguments
+        /// </summary>
+        /// <returns>a string that tells the user they didn't specify any 
+        /// arguments, a character to put on either side of the string, and a 
+        /// number of times the character will be printed</returns>
         private static string Decor()
         {
            return Decor("User did not specify args!", '=', 3);
