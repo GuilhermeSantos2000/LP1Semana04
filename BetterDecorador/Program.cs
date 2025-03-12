f2fa343483816f17f50ae4b7ee0ed53207ab78e2
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-12 19:08:23 +0000

    initial commit

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            char symbol = char.Parse(args[1]);
+            int times = int.Parse(args[2]);
+            Console.WriteLine(Decor(args[0], symbol, times));
+        }  
+        private static string Decor(string s, char dec, int num)
+        {
+            return $"{num*dec} {s} {num*dec}";
+        } 
+    }
+}
